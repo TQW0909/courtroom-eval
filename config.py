8bbd721f3663d0422fe37cb32e89b44f28617485
Tingqi Wang <twang356@usc.edu>
2026-04-13 23:22:00 -0700

    Updated local model names

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from langchain_ollama import ChatOllama
 from langchain_openai import ChatOpenAI
 
 def get_model(model_name: str):
-    local_models = {"llama3:8b-instruct-q4_K_M", "mistral:7b", "qwen2.5:14b"} # TODO: Update with actual local models 
+    local_models = {"llama3:8b-instruct-q4_K_M", "mistral:7b-instruct-q4_K_M ", "qwen2.5:14b-instruct-q4_K_M"} # TODO: Update with actual local models 
     if model_name in local_models:
         return ChatOllama(model=model_name, temperature=0.2)
     else:
